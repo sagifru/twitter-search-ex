60981b4c4b3b10c1ea6af5c1626d6da8cf15eccb
Sagifru <sagifru@gmail.com>
2017-01-13 14:13:28 +0200

    Implement test application with simple UI

--- TwitterSearchTestApp/app/src/main/java/com/example/sagi/twittersearchtestapp/MainActivity.java
+++ TwitterSearchTestApp/app/src/main/java/com/example/sagi/twittersearchtestapp/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.sagi.twittersearchtestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.twitter_search.OAuthHandler;
+import com.example.twitter_search.TwitterSearch;
+import com.example.twitter_search.model.Tweet;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CONSUMER_KEY = "98ZDjZnG8nr9a95aRA2mducVG";
+    private static final String CONSUMER_SECRET = "pUkGxH6RY3jYv910egOaoSvXRjKcBqfkveMueYRDsvN9fNWANQ";
 
     private EditText mQueryEditTxt;
+    private Button mQueryBtn;
     private RecyclerView mTweetsLv;
+    private TweetAdapter mTweetAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mQueryEditTxt = (EditText) findViewById(R.id.query_edt);
+        mQueryBtn = (Button) findViewById(R.id.query_btn);
         mTweetsLv = (RecyclerView) findViewById(R.id.tweets_lv);
+
+        mTweetAdapter = new TweetAdapter(this);
+        mTweetsLv.setAdapter(mTweetAdapter);
+
+        // Start by fetching OAuth token which we'll need to use - for it to be available for us
+        OAuthHandler.getInstance().generateNewToken(getApplicationContext());
+
+        // Start searchin when pressing on the search button
+        mQueryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchString = mQueryEditTxt.getText().toString();
+                startSearch(searchString);
+            }
+        });
+    }
+
+    private void startSearch(final String searchQuery) {
+        TwitterSearch search = new TwitterSearch(CONSUMER_KEY, CONSUMER_SECRET);
+        search.executeSearch(this, searchQuery, new TwitterSearch.TwitterSearchListener() {
+            @Override
+            public void onSearchResult(final List<Tweet> resultTweets) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Update the adapter
+                        mTweetAdapter.updateTweetData(resultTweets);
+                        mTweetAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+
+            @Override
+            public void onSearchError(Exception ex) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Failed using twitter API", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
--- TwitterSearchTestApp/app/src/main/java/com/example/sagi/twittersearchtestapp/TweetAdapter.java
+++ TwitterSearchTestApp/app/src/main/java/com/example/sagi/twittersearchtestapp/TweetAdapter.java
@@ -1,25 +1,61 @@
 package com.example.sagi.twittersearchtestapp;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.twitter_search.model.Tweet;
+
+import java.util.List;
 
 /**
  * Created by Sagi on 1/13/2017.
  * Adapter to connect the tweets list with its relevant UI
  */
 public class TweetAdapter extends RecyclerView.Adapter<TweetViewHolder> {
+    private List<Tweet> mTweetList;
+    private LayoutInflater mInflater;
+    private Context mContext;
+
+    public TweetAdapter(Context ctx) {
+        mContext = ctx;
+        mInflater = LayoutInflater.from(ctx);
+    }
+
     @Override
     public TweetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = mInflater.inflate(R.layout.tweet_item, parent, false);
+
+        return new TweetViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(TweetViewHolder holder, int position) {
+        // Fill the data from the relevant tweet
+        Tweet tweet = mTweetList.get(position);
+        holder.tweetTxt.setText(tweet.getText());
+        holder.userNameTxt.setText(tweet.getUserName());
 
+        // Set date - show how long ago the tweet was
+        long currTime = System.currentTimeMillis();
+        CharSequence formatDate = DateUtils.getRelativeDateTimeString(mContext, tweet.getCreatedAt().getTime(), DateUtils.MINUTE_IN_MILLIS, DateUtils.WEEK_IN_MILLIS,
+                DateUtils.FORMAT_ABBREV_ALL);
+        holder.dateTxt.setText(formatDate);
     }
 
     @Override
     public int getItemCount() {
+        if (mTweetList != null) {
+            return mTweetList.size();
+        }
+
         return 0;
+    }
+
+    public void updateTweetData(List<Tweet> tweetList) {
+        mTweetList = tweetList;
     }
 }
